a5c4e7cdc8c7d27d448c08457a4ff388b928ab9b
Danielle Maywood <danielle@maywood.me>
2023-10-13 12:29:55 +0100

    Add favicon, update font

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import prefetch from "@astrojs/prefetch";
 
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://maywood.dev",
-  integrations: [tailwind(), sitemap(), prefetch(), robotsTxt()],
+  integrations: [tailwind(), sitemap(), robotsTxt()],
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -7,7 +9,11 @@ export default {
       md: "768px",
       lg: "1024px",
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["'IBM Plex Sans Condensed'", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [
     require("@catppuccin/tailwindcss")({
