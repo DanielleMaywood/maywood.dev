d52a29c93aa096bbc7005bfdfbaa71d193c337e9
Danielle Maywood <danielle@maywood.me>
2023-10-13 10:33:51 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
+
+import robotsTxt from "astro-robots-txt";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://maywood.dev",
+  integrations: [tailwind(), sitemap(), prefetch(), robotsTxt()],
+});
