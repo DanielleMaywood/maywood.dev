629e210123dd805c93a8f42d6f672634c81681f7
Danielle Maywood <danielle@maywood.me>
2023-10-27 22:13:14 +0100

    Add prefetching to header links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import markdoc from "@astrojs/markdoc";
 
@@ -9,5 +10,5 @@ import robotsTxt from "astro-robots-txt";
 // https://astro.build/config
 export default defineConfig({
   site: "https://maywood.dev",
-  integrations: [tailwind(), sitemap(), robotsTxt(), markdoc()],
+  integrations: [tailwind(), prefetch(), sitemap(), robotsTxt(), markdoc()],
 });
