a5ac0437ec5b8cbd0ee9beb0f259779e1fc63532
Danielle Maywood <danielle@themaywoods.com>
2023-12-06 00:06:06 +0000

    Use astro 4.0 builtin prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import markdoc from "@astrojs/markdoc";
 
@@ -10,5 +9,6 @@ import robotsTxt from "astro-robots-txt";
 // https://astro.build/config
 export default defineConfig({
   site: "https://maywood.dev",
-  integrations: [tailwind(), prefetch(), sitemap(), robotsTxt(), markdoc()],
+  prefetch: true,
+  integrations: [tailwind(), sitemap(), robotsTxt(), markdoc()],
 });
