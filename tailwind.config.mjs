bf1beed3849a1d172f42a11473f63f09941b458c
Danielle Maywood <danielle@themaywoods.com>
2023-12-27 01:57:54 +0000

    Update theme to neobrutalism

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
+const plugin = require("tailwindcss/plugin");
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -10,13 +11,34 @@ export default {
       lg: "1024px",
     },
     extend: {
+      colors: {
+        neo: {
+          pink: "#ff69b4",
+          purple: "#ffb2ef",
+          blue: "#69d2e7",
+          green: "#90ee90",
+          yellow: "#f4d730",
+        }
+      },
+      boxShadow: {
+        solid: "5px 5px #000"
+      },
       fontFamily: {
-        sans: ["'IBM Plex Sans'", ...defaultTheme.fontFamily.sans],
+        sans: ["'Space Grotesk Variable'", ...defaultTheme.fontFamily.sans],
         mono: ["'IBM Plex Mono'", ...defaultTheme.fontFamily.mono],
       },
     },
   },
   plugins: [
     require("@tailwindcss/typography"),
+    plugin(function ({ addComponents, theme }) {
+      addComponents({
+        '.box': {
+          padding: `${theme("spacing.3")} ${theme("spacing.4")}`,
+          border: "4px solid black",
+          boxShadow: "5px 5px #000"
+        }
+      })
+    })
   ],
 };
