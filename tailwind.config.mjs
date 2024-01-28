ebd3282057d6448f658c6fd462b376ea1a232b03
Danielle Maywood <danielle@themaywoods.com>
2024-01-28 12:03:22 +0000

    Update theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,4 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
-const plugin = require("tailwindcss/plugin");
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -12,41 +11,18 @@ export default {
     },
     extend: {
       colors: {
-        neo: {
-          mustard: "#ffdb58",
-          "cotton-candy": "#ffb2ef",
-          seagull: "#87ceeb",
-          "pastel-green": "#90ee90",
-          anakiwa: "#7df9ff",
-          "mona-lisa": "#ff9494",
+        trans: {
+          white: "#fff",
+          black: "#111",
           pink: "#f5a9b8",
           blue: "#5bcefa",
         },
       },
-      boxShadow: {
-        solid: "5px 5px #000",
-      },
-      fontFamily: {
-        sans: ["'Space Grotesk Variable'", ...defaultTheme.fontFamily.sans],
-        mono: ["'iA Writer Mono'", ...defaultTheme.fontFamily.mono],
-      },
+      // fontFamily: {
+      //   sans: ["'Space Grotesk Variable'", ...defaultTheme.fontFamily.sans],
+      //   mono: ["'iA Writer Mono'", ...defaultTheme.fontFamily.mono],
+      // },
     },
   },
-  plugins: [
-    require("@tailwindcss/typography"),
-    plugin(function ({ addComponents, theme }) {
-      addComponents({
-        ".box": {
-          padding: `${theme("spacing.3")} ${theme("spacing.4")}`,
-          border: "4px solid black",
-          boxShadow: "5px 5px #000",
-        },
-        ".box-lg": {
-          padding: `${theme("spacing.3")} ${theme("spacing.4")}`,
-          border: "4px solid black",
-          boxShadow: "8px 8px #000",
-        },
-      });
-    }),
-  ],
+  plugins: [require("@tailwindcss/typography")],
 };
