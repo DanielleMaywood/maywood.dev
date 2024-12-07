235029cddf08f68510beecf5f9af2efc8337dad8
Danielle Maywood <danielle@themaywoods.com>
2024-12-07 20:34:00 +0000

    add more icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,13 @@ import robotsTxt from "astro-robots-txt";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://daniellemaywood.uk",
-	integrations: [icon(), sitemap(), robotsTxt()],
+	integrations: [
+		icon({
+			include: {
+				ph: ["*"],
+			},
+		}),
+		sitemap(),
+		robotsTxt(),
+	],
 });
